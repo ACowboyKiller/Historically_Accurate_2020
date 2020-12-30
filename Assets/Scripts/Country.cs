453560d2bc0d6c42e4cdb814c994277411f526ad
Chris Walka <walka.christopher@gmail.com>
2020-12-30 17:24:24 -0600

    Big Functionality Push
    - Added Keyboard Input
    - Added Input Commands
    - Added Title, USA, & USSR Models
    - Added Post Processing
    - Began Working on Game Manager

---
+++ Assets/Scripts/Country.cs
@@ -0,0 +1,227 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Country : MonoBehaviour
+{
+
+    #region --------------------    Public Enumerations
+
+
+
+    #endregion
+
+    #region --------------------    Public Events
+
+
+
+    #endregion
+
+    #region --------------------    Public Properties
+
+    /// <summary>
+    /// Returns the name of the country
+    /// </summary>
+    public GameManager.CountryName countryName => _countryName;
+
+    /// <summary>
+    /// Stores whether or not the country is controlled by the player
+    /// </summary>
+    public bool isPlayerControlled { get; set; } = false;
+
+    /// <summary>
+    /// Returns the number of workforce tokens the player has
+    /// </summary>
+    public int workforceTokens => _workforceTokens;
+
+    #endregion
+
+    #region --------------------    Public Methods
+
+    /// <summary>
+    /// Initializes the country
+    /// </summary>
+    public void Initialize(bool _pIsPlayerControlled = false)
+    {
+        isPlayerControlled = _pIsPlayerControlled;
+        _StreamPride();
+        _StreamFunding();
+    }
+
+    /// <summary>
+    /// Purchase a workforce token
+    /// </summary>
+    public bool BuyWorkForceToken()
+    {
+        if (_fundingPoints < _tokenCost) return false;
+        _fundingPoints -= _tokenCost;
+        _tokenCost *= 2;
+        _workforceTokens++;
+        /// TODO:   Play some animation
+        return true;
+    }
+
+    /// <summary>
+    /// Consumes a workforce token
+    /// </summary>
+    public bool UseWorkForceToken()
+    {
+        if (_workforceTokens <= 0) return false;
+        _workforceTokens--;
+        /// TODO:   Play some animation
+        return true;
+    }
+
+    /// <summary>
+    /// Performs some animation whenever a launch is completed
+    /// </summary>
+    public void CompleteLaunch(bool _pSuccess = false)
+    {
+        /// TODO:   Play some animation
+    }
+
+    /// <summary>
+    /// Performs some animation whenever a public test is completed
+    /// </summary>
+    public void CompleteTest(bool _pSuccess = false)
+    {
+        /// TODO:   Play some animation
+    }
+
+    /// <summary>
+    /// Performs some animation whenever a research report is completed
+    /// </summary>
+    public void CompleteReport(bool _pSuccess = false)
+    {
+        /// TODO:   Play some animation
+    }
+
+    /// <summary>
+    /// Performs some animation whenever propaganda is completed
+    /// </summary>
+    public void CompletePropaganda(bool _pSuccess = false)
+    {
+        /// TODO:   Play some animation
+    }
+
+    /// <summary>
+    /// Performs some animation whenever an expirement is completed and updates the research point total
+    /// </summary>
+    public void CompleteExperiment(bool _pSuccess = false)
+    {
+        _researchPoints += _researchValue;
+        /// TODO:   Play some animation
+    }
+
+    /// <summary>
+    /// Begins the launch qte
+    /// </summary>
+    /// <param name="_pbool"></param>
+    public void LaunchQTE(bool _pbool = false)
+    {
+
+    }
+
+    /// <summary>
+    /// Begins the test qte
+    /// </summary>
+    /// <param name="_pbool"></param>
+    public void TestQTE(bool _pbool = false)
+    {
+
+    }
+
+    /// <summary>
+    /// Begins the report qte
+    /// </summary>
+    /// <param name="_pbool"></param>
+    public void ReportQTE(bool _pbool = false)
+    {
+
+    }
+
+    /// <summary>
+    /// Begins the propaganda qte
+    /// </summary>
+    /// <param name="_pbool"></param>
+    public void PropagandaQTE(bool _pbool = false)
+    {
+
+    }
+
+    /// <summary>
+    /// Begins the experiment qte
+    /// </summary>
+    /// <param name="_pbool"></param>
+    public void ExperimentQTE(bool _pbool = false)
+    {
+
+    }
+
+    #endregion
+
+    #region --------------------    Private Fields
+
+    private int _level = 0;
+
+    [SerializeField] GameManager.CountryName _countryName = GameManager.CountryName.USA;
+
+    [SerializeField] private int _nationalPride = 0;
+    [SerializeField] private int _prideStream = 0;
+    private float _prideTimer = 0f;
+
+    [SerializeField] private int _fundingPoints = 0;
+    [SerializeField] private int _fundingStream = 0;
+    private float _fundingTimer = 0f;
+
+    [SerializeField] private int _researchPoints = 0;
+    [SerializeField] private int _researchValue = 0;
+
+    [SerializeField] private int _workforceTokens = 3;
+    [SerializeField] private int _tokenCost = 1;
+
+    #endregion
+
+    #region --------------------    Private Methods
+
+    /// <summary>
+    /// Adds / Subtracts national pride and checks to see if pride has reached 0
+    /// </summary>
+    private void _StreamPride()
+    {
+        //  Breakout if game is not active
+        if (GameManager.state != GameManager.GameState.Gameplay) return;
+
+        //  Configure timer
+        _prideTimer = 0f;
+        DOTween.To(() => _prideTimer, x => _prideTimer = x, 1f, 1f).OnComplete(_StreamPride);
+
+        //  Perform Stream
+        _nationalPride = Mathf.Clamp(_prideStream + _nationalPride, 0, 1000);
+        /// TODO:   Play some animation
+        
+        //  Check for game end
+        if (_nationalPride == 0) GameManager.instance.LoseGame(this);
+    }
+
+    /// <summary>
+    /// Adds / Subtracts funding stream from points
+    /// </summary>
+    private void _StreamFunding()
+    {
+        //  Breakout if game is not active
+        if (GameManager.state != GameManager.GameState.Gameplay) return;
+
+        //  Configure timer
+        _fundingTimer = 0f;
+        DOTween.To(() => _fundingTimer, x => _fundingTimer = x, 1f, 1f).OnComplete(_StreamFunding);
+
+        //  Perform stream
+        _fundingPoints = Mathf.Clamp(_fundingPoints + _fundingStream, 0, 99999);
+        /// TODO:   Play some animation
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scripts/GameTextInput.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GameTextInput : MonoBehaviour
+{
+
+    #region --------------------    Public Properties
+
+    /// <summary>
+    /// Used to set the default text value for labels based off of the object's name
+    /// </summary>
+    public static Dictionary<string, Dictionary<GameManager.CountryName, string>> textValues = new Dictionary<string, Dictionary<GameManager.CountryName, string>>
+    {
+        { "LaunchLabel", new Dictionary<GameManager.CountryName, string> { { GameManager.CountryName.USA, "LAUNCH" }, { GameManager.CountryName.USSR, "ZAPUSK" } } },
+        { "TestLabel", new Dictionary<GameManager.CountryName, string> { { GameManager.CountryName.USA, "PUBLIC TEST" }, { GameManager.CountryName.USSR, "OTSENIVAT'" } } },
+        { "ReportLabel", new Dictionary<GameManager.CountryName, string> { { GameManager.CountryName.USA, "RESEARCH REPORT" }, { GameManager.CountryName.USSR, "OTCHET" } } },
+        { "PropagandaLabel", new Dictionary<GameManager.CountryName, string> { { GameManager.CountryName.USA, "PROPAGANDA" }, { GameManager.CountryName.USSR, "-" } } },
+        { "ExperimentLabel", new Dictionary<GameManager.CountryName, string> { { GameManager.CountryName.USA, "PERFORM EXPERIMENT" }, { GameManager.CountryName.USSR, "ISSLEDOVANIYE" } } }
+    };
+
+    #endregion
+
+    #region --------------------    Public Methods
+
+
+
+    #endregion
+
+    #region --------------------    Private Fields
+
+    /// <summary>
+    /// The text input for the game text input
+    /// </summary>
+    [SerializeField] private InputField _textInput = null;
+
+    /// <summary>
+    /// The previously input text value
+    /// </summary>
+    private string _textValueCache = "";
+
+    /// <summary>
+    /// The list of all available text labels
+    /// </summary>
+    [SerializeField] private List<ActionLabel> _textLabels = new List<ActionLabel>();
+
+    #endregion
+
+    #region --------------------    Private Methods
+
+    /// <summary>
+    /// Perform startup config
+    /// </summary>
+    private void Awake()
+    {
+        _textInput.onValueChanged.AddListener(_RestrictTextInput);
+        _textInput.onValueChanged.AddListener(_UpdateLabelVisuals);
+        _textInput.onValueChanged.AddListener(_CheckForCompleteInput);
+    }
+
+    /// <summary>
+    /// Selects the locking text field
+    /// </summary>
+    private void Start() => _textInput.Select();
+
+    /// <summary>
+    /// Restricts the value of the text box to base values for the labels listed
+    /// </summary>
+    /// <param name="_pValue"></param>
+    private void _RestrictTextInput (string _pValue)
+    {
+        //  Clear out cache if emptying text input
+        _textValueCache = (_pValue == "") ? "" : _textValueCache;
+
+        //  Breakout if the value is unchanged
+        if (_pValue == _textValueCache || _pValue == "") return;
+
+        //  Used for determining if the text value will be reset to cache or if it will hold
+        bool _isValid = false;
+
+        //  Get textValue Keys
+        foreach (KeyValuePair<string, Dictionary<GameManager.CountryName, string>> k in textValues)
+        {
+            _isValid = _isValid || ((_textLabels.Exists(t => t.name == k.Key)) && textValues[k.Key][GameManager.playerCountry.countryName].StartsWith(_pValue.ToUpper()));
+        }
+
+        //  Reset the value or maintain if it's valid
+        _textValueCache = (_isValid) ? _pValue : _textValueCache;
+        _textInput.text = _textValueCache;
+    }
+
+    /// <summary>
+    /// Updates the visuals the appropriate labels
+    /// </summary>
+    /// <param name="_pValue"></param>
+    private void _UpdateLabelVisuals(string _pValue)
+    {
+        //  Update value to match cache
+        _pValue = _textValueCache;
+
+        //  Update the labels to display input progression
+        _textLabels.ForEach(l =>
+        {
+            l.textField.text = (textValues[l.name][GameManager.playerCountry.countryName].StartsWith(_pValue.ToUpper())) ?
+                $"<color=#{ColorUtility.ToHtmlStringRGBA(l.fillColor)}>{_pValue.ToUpper()}</color>{textValues[l.name][GameManager.playerCountry.countryName].Substring(_pValue.Length)}" :
+                textValues[l.name][GameManager.playerCountry.countryName];
+        });
+    }
+
+    /// <summary>
+    /// Checks for completeness of the text input
+    /// </summary>
+    /// <param name="_pValue"></param>
+    private void _CheckForCompleteInput(string _pValue)
+    {
+        //  Update value to match cache
+        _pValue = _textValueCache;
+
+        //  Breakout if the value is unchanged
+        if (_pValue == "") return;
+
+        //  Reset text input if applicable
+        ActionLabel _label = _textLabels.Find(l => textValues[l.name][GameManager.playerCountry.countryName] == _pValue.ToUpper());
+        _textInput.text = _label != null ? "" : _textValueCache;
+        _label?.Invoke();
+
+    }
+
+    #endregion
+
+}
